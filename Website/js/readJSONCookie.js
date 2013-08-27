937436a146faebf7a039b22cd14a5f205de33125
tbkh91 <tbkh91@gmail.com>
2013-08-27 18:39:50 +1000

    Examples for JSON Object
    Added explanation for usage of the JSON object. This should pretty
    clearly outline how to access the different data required to display
    source code and errors.

--- Website/js/readJSONCookie.js
+++ Website/js/readJSONCookie.js
@@ -1,16 +1,47 @@
 if ($.cookie("jsonObjectHtml")) {
-	var jsonString = $.cookie("jsonObjectHtml");
-	// jsonString = jsonString.replace(/\\\//g, "/");
-	jsonString = jsonString.replace(/\\\"/g, '"');
+
+
 	
-	// required to remove quotes before parsing JSON
+	var jsonString = $.cookie("jsonObjectHtml");
+	
+	// fix quotations before attempting to parse
+	jsonString = jsonString.replace(/\\\"/g, '"');
 	jsonString = jsonString.substring(1, jsonString.length-1);
 	
-	alert(jsonString);
-	
-	
 	var jsonObject = $.parseJSON(jsonString);
-	alert(jsonObject[0].source);
 	
-	// show source code and errors etc.
+	// once cookie is read and parsed, it can be deleted
+	$.removeCookie("jsonObjectHtml");
+	
+	/*
+	======= EXAMPLES OF USE ======
+	
+	
+	-- Access first source code of first file (and at this point only file) --
+	jsonObject[0].source; -> returns string
+	
+	-- Access error message of first error of first file --
+	jsonObject[0].errors[0].message; -> returns string
+	
+	-- Access error type of third error of first file --
+	jsonObject[0].errors[2].type; -> returns string
+	
+	-- Access line number of first error of first file --
+	jsonObject[0].errors[0].line; -> returns integer
+	
+	-- Access column number of first error of first file --
+	jsonObject[0].errors[0].col; -> returns integer
+	
+	********* Still to be added to JSON Object **********
+	*****************************************************
+	-- Access error count for first file --
+	jsonObject[0].errorCount; -> returns integer
+	*****************************************************
+	
+	
+	======= END OF EXAMPLES ======
+	*/
+	
+	
+	// ========== Simon add your code here! ======
 }
