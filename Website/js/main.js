527c0e883862722f1322ded54ef2171d2088dcc2
simonghales <simonghales@gmail.com>
2013-09-14 22:40:12 +1000

    "ready" for user testing

--- Website/js/main.js
+++ Website/js/main.js
@@ -57,48 +57,6 @@
 				
 				event.preventDefault();
 			});
-			
-			/* Detect if 'U' is pressed then switch to upload tab */
-			$(document).keydown(function(event){
-				if(event.which == 85){
-					$("#uploadLink").click();
-				}
-			});
-			
-			/* Detect if 'H' is pressed then switch to home tab */
-			$(document).keydown(function(event){
-				if(event.which == 72){
-					$("#homeLink").click();
-				}
-			});
-			
-			/* Detect if 'B' is pressed then switch to home tab */
-			$(document).keydown(function(event){
-				if(event.which == 66){
-					$("#breakdownLink").click();
-				}
-			});
-			
-			/* Detect if 'P' is pressed then switch to home tab */
-			$(document).keydown(function(event){
-				if(event.which == 80){
-					$("#sourceLink").click();
-				}
-			});
-			
-			/* Detect if 'E' is pressed then switch to home tab */
-			$(document).keydown(function(event){
-				if(event.which == 69){
-					$("#errorsLink").click();
-				}
-			});
-			
-			/* Detect if 'F' is pressed then switch to home tab */
-			$(document).keydown(function(event){
-				if(event.which == 20){
-					$("#structureLink").click();
-				}
-			});
 		
 			/* Hide sections of the site that should not be shown initially*/
 			$('.extraOne').hide();
--- Website/site_downloads/old_main.js
+++ Website/site_downloads/old_main.js
@@ -26,3 +26,45 @@
 				$(this).addClass('currentLocation');
 				openErrors();
 			 });
+			 
+			 /* Detect if 'U' is pressed then switch to upload tab */
+			$(document).keydown(function(event){
+				if(event.which == 85){
+					$("#uploadLink").click();
+				}
+			});
+			
+			/* Detect if 'H' is pressed then switch to home tab */
+			$(document).keydown(function(event){
+				if(event.which == 72){
+					$("#homeLink").click();
+				}
+			});
+			
+			/* Detect if 'B' is pressed then switch to home tab */
+			$(document).keydown(function(event){
+				if(event.which == 66){
+					$("#breakdownLink").click();
+				}
+			});
+			
+			/* Detect if 'P' is pressed then switch to home tab */
+			$(document).keydown(function(event){
+				if(event.which == 80){
+					$("#sourceLink").click();
+				}
+			});
+			
+			/* Detect if 'E' is pressed then switch to home tab */
+			$(document).keydown(function(event){
+				if(event.which == 69){
+					$("#errorsLink").click();
+				}
+			});
+			
+			/* Detect if 'F' is pressed then switch to home tab */
+			$(document).keydown(function(event){
+				if(event.which == 20){
+					$("#structureLink").click();
+				}
+			});
