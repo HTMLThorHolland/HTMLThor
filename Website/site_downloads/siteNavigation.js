38e9d07ef427eb042993321b9652c4ce0dabe1d0
simonghales <simonghales@gmail.com>
2013-09-14 12:32:50 +1000

    Help Top Bar

---
+++ Website/site_downloads/siteNavigation.js
@@ -0,0 +1,7 @@
+$(document).mouseup(function (e){	console.log("a click");    var container = $(".topHelpBar");    if (!container.is(e.target) // if the target of the click isn't the container...        && container.has(e.target).length === 0) // ... nor a descendant of the container    {        container.slideToggle();    }});
+$(document).ready(function() {
+	$('.topHelpButt').click(function(e) {
+		e.preventDefault();
+		$('.topHelpKey').slideToggle();
+	 });
+});
