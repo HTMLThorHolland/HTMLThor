880538af2c642c2f869d2040909436669e7f6f0e
tbkh91 <tbkh91@gmail.com>
2013-10-11 16:38:15 +1000

    Compile/class

--- Website/includes/javacompile.sh
+++ Website/includes/javacompile.sh
@@ -1,7 +1,1 @@
-javac -target 1.6 -classpath ../WEB-INF/lib/json-simple-1.1.1.jar -d ../WEB-INF/classes SectionCheck.java
-javac -target 1.6 -classpath ../WEB-INF/lib/mysql-connector-java-3.0.10-stable-bin.jar -d ../WEB-INF/classes MysqlFunctions.java
-javac -target 1.6 -d ../WEB-INF/classes Encapsulation.java
-javac -target 1.6 -d ../WEB-INF/classes RequiredTags.java
-javac -target 1.6 -d ../WEB-INF/classes ValidAttributes.java
-javac -target 1.6 -d ../WEB-INF/classes SingularTags.java
-javac -target 1.6 -d ../WEB-INF/classes SingularID.java
+javac -target 1.6 -classpath ../WEB-INF/lib/json-simple-1.1.1.jar:../WEB-INF/lib/mysql-connector-java-3.0.10-stable-bin.jar -d ../WEB-INF/classes RequiredTags.java SingularID.java Mysqlfunctions.java SectionCheck.java
--- Website/includes/mysqlfunctions.java
+++ Website/includes/mysqlfunctions.java
@@ -11,7 +11,7 @@ import java.sql.Statement;
 /*
 
 */
-public class MysqlFunctions {
+public class Mysqlfunctions {
 
 	private static ResultSet ConnectDB(String Q) {
 
